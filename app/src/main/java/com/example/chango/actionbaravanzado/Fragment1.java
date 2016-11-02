214e5f963bb4810a98e193b1d53204a21d20052e
alumnoIdat <leomanarael@gmail.com>
2016-11-02 09:58:11 -0500

    Initial commit y terminado

---
+++ app/src/main/java/com/example/chango/actionbaravanzado/Fragment1.java
@@ -0,0 +1,32 @@
+package com.example.chango.actionbaravanzado;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Chango on 02/11/2016.
+ */
+
+public class Fragment1 extends Fragment {
+    public static Fragment1 newInstance() {
+        Fragment1 fragment = new Fragment1();
+        return fragment;
+    }
+    public Fragment1() {
+// Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+}
